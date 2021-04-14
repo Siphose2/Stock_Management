48eab35af0f819dfde12746ac36de943aad5a965
Siphose2 <siphosethurinna.kanandawo>
2021-04-14 12:47:39 +0200

    first commit

---
+++ Stock Management/MyJavaScript.js
@@ -0,0 +1,183 @@
+﻿var products = new Array();
+var sales = new Array(); // store emails
+
+function addProduct(name, qty, price) {
+    // create product
+    var newProduct = {
+        productName: name,
+        quantity: qty,
+        productPrice: price
+    };
+
+    // add product to array
+    products.push(newProduct); 
+}
+
+function newProduct() {
+    //var value = document.getElementById("<%=DropDownList1.ClientID%>");
+    var value = document.getElementById('DropDownList1');
+    var getvalue = value.options[value.selectedIndex].value;
+    var pName = value.options[value.selectedIndex].text;  
+
+    var qty = document.getElementById('QuantityTextBox').value;
+    var price = document.getElementById('PriceTextBox').value;
+
+    if (pName == "Select item...") {
+        alert("Please select a product from dropdown.");
+        return;
+    } 
+
+    if (qty === "" || price === null) {
+        alert("Please enter all product details.");
+        return;
+    }
+
+     addProduct(pName, qty, price);
+
+    // reset fields
+    document.getElementById('DropDownList1').selectedIndex = 0;
+    document.getElementById('QuantityTextBox').value = "";
+    document.getElementById('PriceTextBox').value = "";
+
+    //  show arlet that product is added
+    alert(pName + " is Added to Stock.");
+
+    // list products
+    listProducts();
+    updateStats();
+}
+
+
+function listProducts() {
+    document.getElementById('productsList').innerHTML = '';
+    var table = '<table class="products_table"> <tr>'
+        + ' <td> <label>No</label></td>'
+        + ' <td> <label>Product Name</label></td>'
+        + ' <td> <label>Qty</label></td>'
+        + ' <td> <label>Price</label></td>'
+        + '</tr>';
+    products.forEach(myFunction);
+
+    function myFunction(product, index) {
+        table += '<tr> <td> <label>'+(index + 1) + '</label></td>'
+            + '<td> <label>' + product.productName + '</label></td>'
+            + '<td> <label>' + product.quantity + '</label></td>'
+            + '<td> <label> R' + product.productPrice + '</label></td> </tr>'; 
+    }
+
+    document.getElementById('productsList').innerHTML = table + '</table>';
+}
+
+function newSale(email, productName, qty) {
+    removeResponse.innerHTML = '';
+    var salesCount = sales.length;
+    var foundEmail = -1;
+    var foundProdName = -1;
+
+    if (salesCount > 0) {
+        // look for email
+        sales.forEach(getEmail);
+        function getEmail(custEmail, index) {
+            if (custEmail === email) {
+                foundEmail = index;
+            }
+        }
+
+        // if found arlert user
+        if (foundEmail > 0) {
+            var response = document.getElementById('removeResponse');
+            removeResponse.innerHTML = " Email (" + email + ") already exist. Try a new email.";
+            return;
+        }
+    }   
+
+    products.forEach(getProduct);
+    function getProduct(product, index) {
+        if (product.productName === productName) {
+            foundProdName = index;
+        }
+    }
+
+    if (foundProdName > -1) {
+       sales.push(email);
+       var curP = products[foundProdName];
+        if ((curP.quantity - qty) === 0) {
+            produts.splice(foundProdName, 1);
+        } else {
+            curP.quantity -= qty;
+        }
+
+        // reset fields
+        document.getElementById('DropDownList2').selectedIndex = 0;
+        document.getElementById('emailTxtB').value = "";
+        document.getElementById('qtyTxtB2').value = "";
+
+        // list products
+        listProducts();
+        return;
+    } else {
+        removeResponse.innerHTML =  productName + " is not in STOCK.";
+    }
+}
+
+function sellProduct() {
+    var custEmail = document.getElementById('emailTxtB').value;
+    var prodDpD = document.getElementById('DropDownList2');
+    var prodName = prodDpD.options[prodDpD.selectedIndex].text;
+    var quantity = document.getElementById('qtyTxtB2').value;
+
+    newSale(custEmail, prodName, quantity);
+    updateStats();
+}
+
+function updateStats() {
+    var product1 = document.getElementById('prod1');
+    var product2 = document.getElementById('prod2');
+    var product3 = document.getElementById('prod3');
+    var p1Qty = 0;
+    var p1Sum = 0;
+    var p1Count = 0;
+    products.forEach(prod1Qty);
+    function prod1Qty(product, index) {
+        if (product.productName === "PRODUCT1") {
+            p1Qty += parseInt(product.quantity);
+            p1Sum += parseInt(product.productPrice);
+            p1Count ++;
+        }
+    } 
+
+    var p2Qty = 0;
+    var p2Sum = 0;
+    var p2Count = 0;
+    products.forEach(prod2Qty);
+    function prod2Qty(product, index) {
+        if (product.productName === "PRODUCT2") {
+            p2Qty += parseInt(product.quantity);
+            p2Sum += parseInt(product.productPrice);
+            p2Count++;
+        }
+    }
+
+    var p3Qty = 0;
+    var p3Sum = 0;
+    var p3Count = 0;
+    products.forEach(prod3Qty);
+    function prod3Qty(product, index) {
+        if (product.productName === "PRODUCT3") {
+            p3Qty += parseInt(product.quantity);
+            p3Sum += parseInt(product.productPrice);
+            p3Count++;
+        }
+    }
+
+    if (p1Count > 0) {
+        product1.innerHTML = 'PRODUCT1 :-  Quantity: ' + p1Qty + '\t  Price Average: R' + parseFloat((p1Sum / p1Count)).toFixed(2) + '<br/>';
+    }
+    if (p2Count > 0) {
+        product2.innerHTML = 'PRODUCT2 :-  Quantity: ' + p2Qty + '\t  Price Average: R' + parseFloat((p2Sum / p2Count)).toFixed(2) + '<br/>';
+    }
+    if (p3Count > 0) {
+        product3.innerHTML = 'PRODUCT3 :-  Quantity: ' + p3Qty + '\t  Price Average: R' + parseFloat((p3Sum / p3Count)).toFixed(2);
+    }
+    
+}
---
+++ Stock Management/index.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Stock_Management.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Stock Management</title>
+    <link href="MyStyleSheet.css" rel="stylesheet" />
+    <script src="MyJavaScript.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+             <div class="add_stock">
+                    <h1>Add Stock</h1>
+                 <table class="add_stock_table">
+                     <tr>
+                         <td>
+                            <label>Product Name</label>
+                         </td>
+                         <td>
+                             <asp:DropDownList ID="DropDownList1" CssClass="drpDL" runat="server">
+                                 <asp:ListItem text="Select item..." value="0"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT1" value="1"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT2" value="2"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT3" value="3"></asp:ListItem>
+                             </asp:DropDownList>
+                         </td>
+                     </tr>
+                      <tr>
+                         <td>
+                             <label>Quantity</label>
+                         </td>
+                         <td>
+                             <asp:TextBox ID="QuantityTextBox" TextMode="Number" CssClass="qty_txtB" runat="server"></asp:TextBox>
+                         </td>
+                     </tr>
+
+                      <tr>
+                         <td>
+                            <label>Price</label>
+                         </td>
+                         <td>
+                             <asp:TextBox ID="PriceTextBox" TextMode="Number"  CssClass="priceTxtB" runat="server"></asp:TextBox>
+                         </td>
+                     </tr>
+
+                     <tr>
+                         <td colspan="2">
+                             <input type="button" value="Add Product" onclick="newProduct()"/>
+                         </td>
+                     </tr>
+                     
+                 </table>
+             </div>
+
+            <div class="remove_stock">
+                    <h1>Remove Stock</h1>
+                
+                
+                <div id="productsList"></div>
+
+                <table>
+                    <tr>
+                        <td> <label>Customer email</label></td>
+                        <td>
+                            <asp:TextBox ID="emailTxtB" TextMode="Email" runat="server"></asp:TextBox>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> <label>Product</label></td>
+                        <td>
+                            <asp:DropDownList ID="DropDownList2" CssClass="drpDL" runat="server">
+                                 <asp:ListItem text="Select item..." value="0"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT1" value="1"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT2" value="2"></asp:ListItem>
+                                 <asp:ListItem text="PRODUCT3" value="3"></asp:ListItem>
+                             </asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> <label>Quantity</label></td>
+                        <td>
+                            <asp:TextBox ID="qtyTxtB2" TextMode="Number" CssClass="qty_txtB" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                             <input type="button" value="Sell Product" onclick="sellProduct()"/>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td colspan="2">
+                             <label id="removeResponse"></label>
+                        </td>
+                    </tr>
+                </table>
+
+             </div>
+
+            <div class="stock_levels_and_Average_prices">
+                 <h1>Stock Levels and Average Prices</h1>
+                 <label id="prod1">PRODUCT1 :-  Quantity: 0   Price Average: R0.00 <br /></label>
+                 <label id="prod2">PRODUCT2 :-  Quantity: 0   Price Average: R0.00 <br /></label>
+                 <label id="prod3">PRODUCT3 :-  Quantity: 0   Price Average: R0.00</label>
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
